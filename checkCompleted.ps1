d80c6e34ae51a6c11bbee0e386a10c6813965269
Riley <riley.childs@yahoo.com>
2023-12-01 08:34:01 -0500

    Created loop count

--- checkCompleted.ps1
+++ checkCompleted.ps1
@@ -5,19 +5,54 @@ $ErrorActionPreference = "Stop"
 if ($env:COMPUTERNAME -eq "RILEYS-COMPUTER") {
     Import-Module "..\modules\ENV.psm1" -Force
     Import-Module "..\modules\error.psm1" -Force
-    $rootDirectory = Get-Location
+
+    $rootDirectory = "$(Get-Location)\CompletedFiles"
+    $LoopCountFile = "$(Get-Location)\checkCompleted-skippedRuns.txt"
 }
 else {
     Import-Module ".\PowershellScripts\modules\ENV.psm1" -Force
     Import-Module ".\PowershellScripts\modules\error.psm1" -Force
+
     $rootDirectory = "C:\CompletedFiles"
+    $LoopCountFile = "C:\PowershellScripts\checkCompleted-skippedRuns.txt"
+}
+
+Function New-LoopFileIfNotExists {
+    if (-not (Test-Path $LoopCountFile)) {
+        New-Item -ItemType File -Path $LoopCountFile
+    }
+}
+
+Function Set-Loop-Process {
+    param(
+        [string]$Mode
+    )
+
+    if ($Mode -eq "reset") {
+        $LoopNo = 0
+        $LoopNo | Out-File -FilePath $LoopCountFile
+    }
+    elseif ($Mode -eq "set") {
+        $LoopNo = [int](Get-Content -Path $LoopCountFile)
+        $LoopNo++
+        $LoopNo | Out-File -FilePath $LoopCountFile
+
+        if ($LoopNo % 4 -eq 0) {
+            Find-Recent -Time $timespan
+        }
+    }
 }
 
 try{
     $timespan = 10
     $recentCompletedFiles = $rootDirectory | Get-ChildItem -File | Where-Object { $_.CreationTime -ge (Get-Date).AddMinutes(-$timespan) }
+   
+    New-LoopFileIfNotExists
+
     if($recentCompletedFiles.Length -eq 0){
-        Find-Recent -Time $timespan
+        Set-Loop-Process -Mode "set"
+    } else {
+        Set-Loop-Process -Mode "reset"
     }
 
 }catch{
