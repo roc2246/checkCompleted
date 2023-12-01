b6643acb1f08cb0c71af2cd10c88fce459259db1
Riley <riley.childs@yahoo.com>
2023-12-01 08:48:29 -0500

    Updated loop process

--- checkCompleted.ps1
+++ checkCompleted.ps1
@@ -6,8 +6,8 @@ if ($env:COMPUTERNAME -eq "RILEYS-COMPUTER") {
     Import-Module "..\modules\ENV.psm1" -Force
     Import-Module "..\modules\error.psm1" -Force
 
-    $rootDirectory = "$(Get-Location)\CompletedFiles"
-    $LoopCountFile = "$(Get-Location)\checkCompleted-skippedRuns.txt"
+    $rootDirectory = Join-Path (Get-Location) "CompletedFiles"
+    $LoopCountFile = Join-Path (Get-Location) "checkCompleted-skippedRuns.txt"
 }
 else {
     Import-Module ".\PowershellScripts\modules\ENV.psm1" -Force
@@ -36,25 +36,24 @@ Function Set-Loop-Process {
         $LoopNo = [int](Get-Content -Path $LoopCountFile)
         $LoopNo++
         $LoopNo | Out-File -FilePath $LoopCountFile
-
-        if ($LoopNo % 4 -eq 0) {
-            Find-Recent -Time $timespan
-        }
+        if (($LoopNo % 4 -eq 0)) { Find-Recent -Time $timespan }
     }
 }
 
-try{
+try {
     $timespan = 10
     $recentCompletedFiles = $rootDirectory | Get-ChildItem -File | Where-Object { $_.CreationTime -ge (Get-Date).AddMinutes(-$timespan) }
    
     New-LoopFileIfNotExists
 
-    if($recentCompletedFiles.Length -eq 0){
+    if ($recentCompletedFiles.Count -eq 0) {
         Set-Loop-Process -Mode "set"
-    } else {
+    }
+    else {
         Set-Loop-Process -Mode "reset"
     }
 
-}catch{
+}
+catch {
     Send-Error -ScriptName $MyInvocation.MyCommand.Name -ErrorParam "$($Error[0].Exception.Message)"
 }
